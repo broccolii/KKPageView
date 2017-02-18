aeb31c9ae421d38e9e448dccfd29255a673bf6ef
wujingpeng <wujingpeng@youzan.com>
2017-02-19 04:48:33 +0800

    remove YZPageTransition & add YZPageTransitionAnimator

---
+++ YZPageView/Resource/YZPageTransitionAnimator.h
@@ -0,0 +1,13 @@
+//
+//  YZPageTransitionAnimator.h
+//  YZPageView
+//
+//  Created by Broccoli on 2017/2/19.
+//  Copyright © 2017年 broccoli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol YZPageTransitionAnimator <NSObject>
+
+@end
