25d0dfd1a819318b50506433bbf75425eb0e189b
wujingpeng <wujingpeng@youzan.com>
2017-02-19 17:57:29 +0800

    Add Initialize method

--- YZPageView/Resource/YZPageCollectionView/YZPageViewItem.m
+++ YZPageView/Resource/YZPageCollectionView/YZPageViewItem.m
@@ -10,4 +10,39 @@
 
 @implementation YZPageViewItem
 
+#pragma mark - Initialize
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (!self) {
+        return nil;
+    }
+    [self commonInit];
+    [self setupUIComponent];
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (!self) {
+        return nil;
+    }
+    [self commonInit];
+    [self setupUIComponent];
+    return self;
+}
+
+- (void)commonInit {
+    self.contentView.backgroundColor = [UIColor whiteColor];
+    self.contentView.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.contentView.layer.shadowRadius = 5;
+    self.contentView.layer.shadowOpacity = 0.75;
+    self.contentView.layer.shadowOffset = CGSizeZero;
+    self.backgroundColor = [UIColor whiteColor];
+}
+
+#pragma mark - setup UI component
+- (void)setupUIComponent {
+    
+}
+
 @end
